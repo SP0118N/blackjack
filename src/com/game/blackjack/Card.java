c846d4ef4049201ae97d7c1242d117c46c7966d5
JoshuaERichardson <joshuarichardson1478@gmail.com>
2023-03-10 10:05:12 -0700

    Add Deck, Card

---
+++ src/com/game/blackjack/Card.java
@@ -0,0 +1,7 @@
+package com.game.blackjack;
+
+enum Card {
+    ACE_SPADES,
+    TWO_SPADES,
+    THREE_SPADES
+}
---
+++ src/com/game/blackjack/Deck.java
@@ -0,0 +1,7 @@
+package com.game.blackjack;
+
+import java.util.Set;
+
+class Deck {
+    private Set<Card> cards;
+}
